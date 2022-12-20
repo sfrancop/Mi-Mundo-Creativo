5b88e79a42dce43861e20deb5fab8597c836d400
Sergio Franco <s.francop@uniandes.edu.co>
2022-12-20 07:49:36 -0500

    Imágenes y mapas en seccion de contacto

--- src/components/Informacion/Informacion.js
+++ src/components/Informacion/Informacion.js
@@ -13,12 +13,12 @@ export default class Informacion extends Component {
         <p className='informacion-container-title'>Niveles</p>
 
         <div className='informacion-container-niveles'>
-            <Nivel nivel="Caminadores" fondo="https://images.pexels.com/photos/6402526/pexels-photo-6402526.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" edadInicial="1" edadFinal="2" unidadInicial="años" unidadFinal="años" descripcion="Lorem ipsum dolor sit amet, , quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." />
-            <Nivel nivel="Caminadores" fondo="https://images.pexels.com/photos/3418400/pexels-photo-3418400.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" edadInicial="1" edadFinal="2" unidadInicial="años" unidadFinal="años" descripcion="Lorem ipsum dolor sit amet, , quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." />
-            <Nivel nivel="Caminadores" fondo="https://images.pexels.com/photos/3045825/pexels-photo-3045825.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" edadInicial="1" edadFinal="2" unidadInicial="años" unidadFinal="años" descripcion="Lorem ipsum dolor sit amet, , quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." />
-            <Nivel nivel="Caminadores" fondo="https://images.pexels.com/photos/3109807/pexels-photo-3109807.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" edadInicial="1" edadFinal="2" unidadInicial="años" unidadFinal="años" descripcion="Lorem ipsum dolor sit amet, , quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." />
-            <Nivel nivel="Caminadores" fondo="https://images.pexels.com/photos/3109821/pexels-photo-3109821.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" edadInicial="1" edadFinal="2"  unidadInicial="años" unidadFinal="años" descripcion="Lorem ipsum dolor sit amet,  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." />
-            <Nivel nivel="Caminadores" fondo="https://images.pexels.com/photos/3109808/pexels-photo-3109808.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" edadInicial="1" edadFinal="2" unidadInicial="años" unidadFinal="años" descripcion="Lorem ipsum dolor sit amet, , quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." />
+            <Nivel nivel="Caminadores" fondo="https://cdn.pixabay.com/photo/2019/03/03/20/23/background-4032775_960_720.png" edadInicial="1" edadFinal="2" unidadInicial="años" unidadFinal="años" descripcion="Lorem ipsum dolor sit amet, , quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." />
+            <Nivel nivel="Caminadores" fondo="https://cdn.pixabay.com/photo/2018/04/06/15/17/pattern-3296033_960_720.png" edadInicial="1" edadFinal="2" unidadInicial="años" unidadFinal="años" descripcion="Lorem ipsum dolor sit amet, , quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." />
+            <Nivel nivel="Caminadores" fondo="https://cdn.pixabay.com/photo/2021/11/22/10/14/floral-6816295_960_720.png" edadInicial="1" edadFinal="2" unidadInicial="años" unidadFinal="años" descripcion="Lorem ipsum dolor sit amet, , quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." />
+            <Nivel nivel="Caminadores" fondo="https://cdn.pixabay.com/photo/2022/05/02/16/17/pattern-7169974_960_720.png" edadInicial="1" edadFinal="2" unidadInicial="años" unidadFinal="años" descripcion="Lorem ipsum dolor sit amet, , quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." />
+            <Nivel nivel="Caminadores" fondo="https://cdn.pixabay.com/photo/2017/01/31/17/55/background-2025984_960_720.png" edadInicial="1" edadFinal="2"  unidadInicial="años" unidadFinal="años" descripcion="Lorem ipsum dolor sit amet,  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." />
+            <Nivel nivel="Caminadores" fondo="https://cdn.pixabay.com/photo/2018/02/24/09/40/pattern-3177667_960_720.png" edadInicial="1" edadFinal="2" unidadInicial="años" unidadFinal="años" descripcion="Lorem ipsum dolor sit amet, , quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." />
         </div>
 
         <div className='informacion-container-matricula'>
@@ -63,7 +63,9 @@ export default class Informacion extends Component {
 
         </div>
 
-
+        <div>
+            <iframe title='mapa' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d32397.80657231896!2d-73.27577993477473!3d10.466536145985492!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e8ab9fe3a84ee77%3A0x6c30e50d14fde4d3!2sJard%C3%ADn%20Infantil%20Mi%20Mundo%20Creativo!5e0!3m2!1ses-419!2sco!4v1671538926648!5m2!1ses-419!2sco" width="100%" height="450" style={{border:0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
 
       </div>
     )
--- src/pages/Estudiantes/Estudiantes.js
+++ src/pages/Estudiantes/Estudiantes.js
@@ -11,9 +11,9 @@ export default class Estudiantes extends Component {
         <Navbar/>
         <p className='informacion-container-title'>Estudiantes</p>
         <div className='informacion-container-niveles'>
-          <Card fondo="https://images.pexels.com/photos/3045825/pexels-photo-3045825.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" titulo="Manuel de convivencia" descripcion="Encuentre y descargue aquí el manual de convivencia del jardín." link="/"/>
-          <Card fondo="https://images.pexels.com/photos/3045825/pexels-photo-3045825.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" titulo="Formatos" descripcion="Encuentre y descargue aquí los formatos de útilidad para el estudiante." link="/"/>
-          <Card fondo="https://images.pexels.com/photos/3045825/pexels-photo-3045825.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" titulo="Calificaciones" descripcion="Encuentre y descargue aquí las calificaciones del estudiante." link="/"/>
+          <Card fondo="https://cdn.pixabay.com/photo/2021/09/21/09/34/background-6642882_960_720.png" titulo="Manuel de convivencia" descripcion="Encuentre y descargue aquí el manual de convivencia del jardín." link="/"/>
+          <Card fondo="https://cdn.pixabay.com/photo/2017/01/17/22/49/background-1988348_960_720.png" titulo="Formatos" descripcion="Encuentre y descargue aquí los formatos de útilidad para el estudiante." link="/"/>
+          <Card fondo="https://cdn.pixabay.com/photo/2019/10/01/13/58/retro-pattern-4518357_960_720.png" titulo="Calificaciones" descripcion="Encuentre y descargue aquí las calificaciones del estudiante." link="/"/>
         </div>
         <Footer/>
       </div>
